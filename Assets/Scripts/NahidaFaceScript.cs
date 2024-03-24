5d1101022cd5e15bf84b1122ccaeee3230b35f54
Kethers <838859010@qq.com>
2024-03-24 21:06:46 +0800

    Init update but bad tonemapping

---
+++ Assets/Scripts/NahidaFaceScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NahidaFaceScript : MonoBehaviour
+{
+	public Transform HeadTrans;
+	public Transform HeadFwd;
+	public Transform HeadRight;
+	private Material[] faceMaterials;
+
+	private void Start()
+	{
+		// faceMaterials = new Material[9];
+	}
+
+	private void Update()
+	{
+		Vector3 fwdVec = HeadFwd.position - HeadTrans.position;
+		Vector3 rightVec = HeadRight.position - HeadTrans.position;
+
+		fwdVec = fwdVec.normalized;
+		rightVec = rightVec.normalized;
+
+		Vector4 fwdVec4 = new Vector4(fwdVec.x, fwdVec.y, fwdVec.z);
+		Vector4 rightVec4 = new Vector4(rightVec.x, rightVec.y, rightVec.z);
+
+		// for (int i = 0; i < faceMaterials.Length; i++)
+		// {
+		// 	Material mat = faceMaterials[i];
+		// 	mat.SetVector("_ForwardVector", fwdVec4);
+		// 	mat.SetVector("_RightVector", rightVec4);
+		// }
+	}
+}
